06dd301c6b0874610b5863d38605da28c5d95bd9
Sean Ashton <sean.ashton@schimera.com>
2013-08-02 12:58:35 +1000

    Added methods to control the LED

--- package/picam/__init__.py
+++ package/picam/__init__.py
@@ -3,7 +3,18 @@
 import _picam
 import StringIO
 from PIL import Image
+import RPi.GPIO as GPIO
 
+#add disable_camera_led=1 to config.txt to have control over the LED
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.OUT, initial=False) 
+
+def LEDOn():
+    GPIO.output(5,True)
+def LEDOff():
+    GPIO.output(5,False)
+    
 def takePhoto():
     s = _picam.takePhoto()
     ss = StringIO.StringIO(s)
